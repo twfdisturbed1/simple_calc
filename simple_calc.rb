a6d91f989bbba9b0c536d02c9bea917908cf2372
William Mullins <twfdisturbed@hotmail.com>
2018-06-11 13:05:40 -0400

    Calc

---
+++ simple_calc.rb
@@ -0,0 +1,25 @@
+puts "Do you want to +(1) or -(2) or *(3) or /(4)"
+symbol = gets.chomp
+
+puts "Enter your first number"
+number_a = gets.chomp
+
+puts "Enter your second number"
+number_b = gets.chomp
+
+puts  "This is your Answer" 
+
+if symbol == "1" then
+    number_c = number_a.to_i + number_b.to_i
+elsif symbol == "2" then
+    number_c = number_a.to_i - number_b.to_i
+elsif symbol == "3" then
+    number_c = number_a.to_i * number_b.to_i
+elsif symbol == "4" then
+    number_c = number_a.to_i / number_b.to_i
+else 
+    number_c = puts "Wrong selection. Please choose From 1-4 for your first choice"    
+end
+
+
+puts number_c
