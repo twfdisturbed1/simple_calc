78af9ad4e63788eba42e7d119b2cacc52a9a67d8
William Mullins <twfdisturbed@hotmail.com>
2018-06-12 04:04:49 -0400

    updated

--- simple_calc.rb
+++ simple_calc.rb
@@ -1,33 +1,51 @@
 puts "Do you want to +(1) or -(2) or *(3) or /(4)"
-symbol = gets.chomp
 
-puts "Enter your first number"
-number_a = gets.chomp
+loop do
+symbol = gets.chomp.to_i
+puts "symbol is #{symbol}"
 
-puts "Enter your second number"
-number_b = gets.chomp
-
+if symbol == 1 then
+    puts "Addition"
+    puts "Enter your first number"
+    number_a = gets.chomp
+    puts "Enter your second number"
+    number_b = gets.chomp
+number_c = number_a.to_i + number_b.to_i
 puts  "This is your Answer" 
-
-if symbol == "1" then
-    number_c = number_a.to_i + number_b.to_i
-elsif symbol == "2" then
-    number_c = number_a.to_i - number_b.to_i
-elsif symbol == "3" then
-    number_c = number_a.to_i * number_b.to_i
-elsif symbol == "4" then
-    number_c = number_a.to_i / number_b.to_i
-else 
-    number_c = puts "Wrong selection. Please choose From 1-4 for your first choice"    
-end
-
-
 puts number_c
-
-puts "Did it work for you? 1 = yes | 2 = no"
-finish = gets.chomp
-if finish == "1" then 
-puts "yay"
+break
+elsif symbol == 2 then
+puts "Subtraction"
+    puts "Enter your first number"
+    number_a = gets.chomp
+    puts "Enter your second number"
+    number_b = gets.chomp
+number_c = number_a.to_i - number_b.to_i
+puts  "This is your Answer" 
+puts number_c
+break
+elsif symbol == 3 then
+    puts "Times"
+    puts "Enter your first number"
+    number_a = gets.chomp
+    puts "Enter your second number"
+    number_b = gets.chomp
+number_c = number_a.to_i * number_b.to_i
+puts  "This is your Answer" 
+puts number_c   
+break 
+elsif symbol == 4 then
+    puts "Division"
+    puts "Enter your first number"
+    number_a = gets.chomp
+    puts "Enter your second number"
+    number_b = gets.chomp
+number_c = number_a.to_i / number_b.to_i
+puts  "This is your Answer" 
+puts number_c
+break
 elsif 
-puts "Oh Well"
+    puts "Wrong Choice"        
+
+end
 end
